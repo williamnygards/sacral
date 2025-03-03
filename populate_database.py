6e8a9f2166a1ae19c2ba5f4e4683ae8ec0124c09
Shaonee <130759267+Shaonee@users.noreply.github.com>
2025-03-03 11:13:47 +0100

    Fixed wrong file upload

--- populate_database.py
+++ populate_database.py
@@ -14,34 +14,31 @@ def load_courses():
     def metadata_func(record: dict, metadata: dict) -> dict:
         metadata["course_code"] = record.get("kurskod").lower()
         metadata["course_name"] = record.get("name").lower()
-        metadata["webpage"] = record.get("url")
         return metadata
     
-    path = "C:\Skola\DVA318_SACRAL\git_code\mdu_data_url\course\courses.jsonl"
+    path = "./MDU_crawler/mdu_data/course/courses.jsonl"
     loader = JSONLoader(file_path=path, json_lines=True, text_content=False, jq_schema=".", metadata_func=metadata_func)
     docs = loader.load()
 
     embed_model = OllamaEmbeddings(model="mxbai-embed-large")
     
-    vector_store = Chroma(embedding_function=embed_model, persist_directory="./chroma_mxbai_embed_large")
-    for i in range(len(docs), 5000):
-        vector_store.add_documents(docs[i:i+5000])
+    vector_store = Chroma(embedding_function=embed_model, persist_directory="./chroma")
+    vector_store.add_documents(docs)
     print("Courses sucessfully added to DB")
 
 def load_programs():
     def metadata_func(record: dict, metadata: dict) -> dict:
         metadata["program_code"] = record.get("programkod").lower()
         metadata["program_name"] = record.get("name").lower()
-        metadata["webpage"] = record.get("url")
         return metadata
     
-    path = "C:\Skola\DVA318_SACRAL\git_code\mdu_data_url\program\programs.jsonl"
+    path = "./MDU_crawler/mdu_data/program/programs.jsonl"
     loader = JSONLoader(file_path=path, json_lines=True, text_content=False, jq_schema=".", metadata_func=metadata_func)
     docs = loader.load()
 
     embed_model = OllamaEmbeddings(model="mxbai-embed-large")
     
-    vector_store = Chroma(embedding_function=embed_model, persist_directory="./chroma_mxbai_embed_large")
+    vector_store = Chroma(embedding_function=embed_model, persist_directory="./chroma")
     vector_store.add_documents(docs)
     print("Programs sucessfully added to DB")
 
